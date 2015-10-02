794616d086f2d631302475ac4ae72e8a8ee56c56
Spitzmueller.Lukas <Lukas98sp@gmail.com>
2015-10-02 09:08:49 +0200

    Bool'sche Operatoren

--- Uebung/src/Uebung.java
+++ Uebung/src/Uebung.java
@@ -4,6 +4,24 @@ public class Uebung {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
 
-}
+			 byte b = 42;
+			 short s = 42;
+			 int i = 42;
+			 long l = 42L;
+			  System.out.println(b);
+			  System.out.println(s);
+			  System.out.println(i);
+			  System.out.println(l);
+			  System.out.println(Byte.MIN_VALUE);
+			  System.out.println(Byte.MAX_VALUE);
+			  System.out.println(Short.MIN_VALUE);
+			  System.out.println(Short.MAX_VALUE);
+			  System.out.println(Integer.MIN_VALUE);
+			  System.out.println(Integer.MAX_VALUE);
+			  System.out.println(Long.MIN_VALUE);
+			  System.out.println(Long.MAX_VALUE);
+			  System.out.println(WahrOderFalsch());
+	}
+	static boolean WahrOderFalsch()
+{return (4==4 && 5==5);}}
