a8e7d3406d3cf8db9bc469cad551610824cfe288
Spitzmueller.Lukas <Lukas98sp@gmail.com>
2015-10-02 08:45:54 +0200

    second commit

---
+++ Uebung/src/Spaß.java
@@ -0,0 +1,9 @@
+
+public class Spaﬂ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println((17+4)*2==42?"tats‰chlich wahr":"unterschiedlich");
+	}
+
+}
